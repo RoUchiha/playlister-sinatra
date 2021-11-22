05f73ff292b0e1855b7a4c08c456eed43feef7db
RoUchiha <swaggsikh@gmail.com>
2021-11-22 01:29:17 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,5 +10,15 @@ class ArtistsController < ApplicationController
         erb :'artists/index'
     end
 
+    get '/artists/new' do
+        
+        erb :'artists/new'
+    end
+
+    get '/artists/:slug' do
+        @artist = Artist.find_by_slug(params[:slug])
+
+        erb :'artists/show'
+    end
 
 end
